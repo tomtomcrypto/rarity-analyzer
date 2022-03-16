7fb4627aa3714bb87fc464c35e2f45749f380bca
popo12331 <101725629+popo12331@users.noreply.github.com>
2022-03-16 20:51:50 +0100

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,9 +8,9 @@ const Footer = () => {
 			<footer className="max-w-screen-xl w-full mx-auto xl:px-0 px-5">
 				<div className="space-x-6 py-6 text-xs w-full flex items-center md:justify-end justify-center text-gray-500 dark:text-gray-300">
 					<div>
-						Made by the{" "}
+						Made by {" "}
 						<a href="https://www.tsquirrel.io/" target="_blank" rel="noreferrer">
-							TSquirrel 
+							TSquirrel NFT
 						</a>
 					</div>
 					<div className="flex items-center justify-center space-x-1">
